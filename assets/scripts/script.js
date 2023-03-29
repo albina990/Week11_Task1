f25659b20a5902adb57da897eb60568c155f6dbd
albina990 <albina.khadieva90@gmail.com>
2023-03-29 13:31:33 +0200

    fix script, css

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,9 @@
-let button = document.querySelector("button")
-let total = document.querySelector(".total__price")
-let prices = Array.from(document.querySelectorAll(".card__price"))
-let pricesInt = prices.map((item) => parseInt(item.textContent))
-let totalPrice = pricesInt.reduce((a, b) => a + b)
-total.textContent = totalPrice + " руб."
+let button = document.querySelector("button");
+let total = document.querySelector(".total__price");
+let prices = Array.from(document.querySelectorAll(".card__price"));
+let pricesInt = prices.map((item) => parseInt(item.textContent));
+let totalPrice = pricesInt.reduce((a, b) => a + b);
+total.textContent = totalPrice + " руб.";
 
 function discount() {
     totalPrice = totalPrice * 0.8
@@ -14,4 +14,4 @@ function discount() {
     button.removeEventListener("click", discount)
 }
 
-button.addEventListener("click", discount)
+button.addEventListener("click", discount);
